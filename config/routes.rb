ea0d1d6afbfb2eef8cb92c6ed783cf772a2feb87
sato.hayashi <sato_hayashi_kazuhiro@yahoo.co.jp>
2020-05-20 12:52:59 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :microposts, only: [:index, :show, :new, :create] 
+  resources :microposts, only: [:index, :show, :new, :create, :destroy] 
   resources :relationships, only: [:create, :destroy]
   resources :favorites, only: [:create, :destroy]
 end
