f2739aed673b82858beb4d0676ba7456a1260556
sato.hayashi <sato_hayashi_kazuhiro@yahoo.co.jp>
2020-05-20 10:31:58 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,16 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.like(micropost)
+    flash[:success] = 'ツイートをお気に入りに登録しました'
+    redirect_to root_url
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unlike(micropost)
+    flash[:success] = 'ツイートのお気に入りを解除しました。'
+    redirect_to root_url
+  end
+end
